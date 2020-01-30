ace5a0081ae90acd43adc44f2ee8a36bc6d8ab2c
Naresh Mantravadi <naresh.mantravadi@tn.gov>
2020-01-30 09:54:30 -0600

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client Side javascript file is loaded!')
-
 const weatherForm = document.querySelector('form')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
