dd7469ae9542acce2f9d25f795731af6dfdcb5dd
Naresh Mantravadi <naresh.mantravadi@tn.gov>
2020-02-04 12:12:58 -0600

    Added High and low temp

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -14,7 +14,8 @@ request({url, json: true}, (error, {body} = response) => {
 
    } else {
     //console.log(response.body.daily.data[0].summary +'It is currently '+response.body.currently.temperature+'degrees out. There is a '+response.body.currently.precipProbability+'% chance of rain')
-    callback(undefined,body.daily.data[0].summary +'It is currently '+body.currently.temperature+'degrees out. There is a '+body.currently.precipProbability+'% chance of rain')
+    //console.log(body.daily.data[0])
+    callback(undefined,body.daily.data[0].summary +'It is currently '+body.currently.temperature+'degrees out. The high today is '+body.daily.data[0].temperatureHigh+' and low of '+body.daily.data[0].temperatureLow+'. There is a '+body.currently.precipProbability+'% chance of rain')
    }
     
  })
